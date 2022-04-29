c3ecca9428cf5fd2b07b5e2fe7e8d06719be934a
Albino Tonnina <albino.tonnina@asos.com>
2022-04-29 12:32:53 +0100

    branches config

--- release.config.js
+++ release.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   branches: [
-    { name: "1.x", range: "1.x", channel: "1.x" }, // Only after the `1.x` is created in the repo
-    { name: "2.x", range: "2.x", channel: "2.x" }, // Only after the `2.x` is created in the repo
     { name: "main" },
-    { name: "next", channel: "next" }, // Only after the `next` is created in the repo
-    { name: "pre/rc", channel: "pre/rc", prerelease: "rc" }, // `prerelease` is built with the template `${name.replace(/^pre\\//g, "")}`
     { name: "beta", channel: "beta", prerelease: true }, // `prerelease` is set to `beta` as it is the value of `name`
   ],
   plugins: [
