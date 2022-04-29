d5e6dc420ad916eeb22ecb64d91fe3660c5cb78e
Albino Tonnina <albino.tonnina@asos.com>
2022-04-29 10:38:20 +0100

    prepare for the shooow

--- release.config.js
+++ release.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   branches: ["main", "next"],
-  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator']
-  // plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
-}
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    // "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    // "@semantic-release/npm",
+    // [
+    //   "@semantic-release/github",
+    //   {
+    //     assets: ["dist/**"],
+    //   },
+    // ],
+  ],
+  dryRun: true,
+  debug: true,
+  ci: false, // Set to false to skip Continuous Integration environment verifications. This allows for making releases from a local machine.
+};
