433066d7f349e8c643ab7beac1b11d9b5f43ad07
Albino Tonnina <albino.tonnina@asos.com>
2022-04-29 10:50:17 +0100

    add first test configuration

--- release.config.js
+++ release.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  branches: ["main", "next"],
+  branches: [
+    { name: "1.x", range: "1.x", channel: "1.x" }, // Only after the `1.x` is created in the repo
+    { name: "2.x", range: "2.x", channel: "2.x" }, // Only after the `2.x` is created in the repo
+    { name: "main" },
+    { name: "next", channel: "next" }, // Only after the `next` is created in the repo
+    { name: "pre/rc", channel: "pre/rc", prerelease: "rc" }, // `prerelease` is built with the template `${name.replace(/^pre\\//g, "")}`
+    { name: "beta", channel: "beta", prerelease: true }, // `prerelease` is set to `beta` as it is the value of `name`
+  ],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "eslint",
+        releaseRules: [
+          { type: "docs", scope: "README", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        },
+      },
+    ],
     // "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     // "@semantic-release/npm",
